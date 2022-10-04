184d7915c686723345ae05b5385e60d17ce07265
Mark <kalmark@mail.ru>
2022-10-04 22:08:14 +0300

    Add Ex_8

---
+++ Ex_8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 0)
+{
+    Console.Write("Четные числа от 1 до " + N + ": ");
+    for (int i = 1; i <=N ;i++)
+    {
+        if (i % 2 == 0) Console.Write(i + " ");
+    }
+}
+else
+{
+    Console.Write("Четные числа от" + N + " до 1: ");
+    for (int i = N; i <=1 ;i++)
+    {
+        if (i % 2 == 0) Console.Write(i + " ");
+    } 
+}
