d66064830c3d3f558d3aec9a5dfedb132f04d210
Mark <kalmark@mail.ru>
2022-10-04 21:47:58 +0300

    Add Ex_6

---
+++ Ex_6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 ==0) Console.WriteLine(a + " - четное число");
+else Console.WriteLine(a + " - нечетное число");
