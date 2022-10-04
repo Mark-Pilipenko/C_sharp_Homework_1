b812c8be77bf4be924bd3134282f5de62a941915
Mark <kalmark@mail.ru>
2022-10-04 21:30:25 +0300

    Add Ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Большее число: " + a);
+}
+else if (a < b)
+{
+    Console.WriteLine("Большее число: " + b);
+}
+else
+{
+    Console.WriteLine("Числа равны: " + a);
+}
