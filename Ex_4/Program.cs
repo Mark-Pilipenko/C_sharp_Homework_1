25b7486aa84096be3f46a9cf3f517533ea33c4f9
Mark <kalmark@mail.ru>
2022-10-04 21:41:45 +0300

    Add Ex_4

---
+++ Ex_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("И еще одно: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное значение: " + max);
